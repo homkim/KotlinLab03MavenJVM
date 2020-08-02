d756fce72f97f006354b48a5f770e3704024c1de
homkim <homkim01@gmail.com>
2020-08-02 20:21:56 +0900

    Initial commit

---
+++ src/main/kotlin/org/example/Hello.kt
@@ -0,0 +1,9 @@
+package org.example
+
+import org.apache.commons.lang3.StringUtils
+
+fun main(args: Array<String>) {
+    val data="Hello World homkim"
+    println(StringUtils.deleteWhitespace(data))
+}
+
